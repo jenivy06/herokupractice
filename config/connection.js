46ce91ceeeeb505a8171bbd1e74274403bd8fe6e
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-06-23 20:02:21 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,19 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "brandy978",
-  database: "burgers_db"
+  database: "JawsDB"
 });
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  })
+}
 
 connection.connect(function(err) {
   if (err) {
@@ -16,4 +27,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 module.exports = connection;
